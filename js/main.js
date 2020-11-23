368a5e7eff1f093d17a0d4dcc3a2c48671aa79a6
aaoshchepkov <aa_oschepkov@mail.ru>
2020-11-23 19:18:47 +0700

    crete: swiper in hotel

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-button__next",
+    prevEl: ".slider-button__prev",
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  
+});
+
+
+
+
