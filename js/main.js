04630aa0e45a9e55ac2ac200eb4d4a321cdeeceb
aaoshchepkov <aa_oschepkov@mail.ru>
2020-12-03 10:33:33 +0700

    fix: log & media menu scroll

--- js/main.js
+++ js/main.js
@@ -30,7 +30,6 @@ var reviewsSlider = new Swiper(".reviews-slider", {
 
 var menuButton = document.querySelector(".menu-button");
 menuButton.addEventListener("click", function () {
-console.log("Клик по кнопке")
 document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
 });
 
